d4cfc3f16aafede41f6c67eeafdd4e4cc045220d
Lypt0x <2xlyptox@gmail.com>
2021-08-28 14:13:13 +0200

    Remove unnecessary feature from serenity

--- src/bot/handler.rs
+++ src/bot/handler.rs
@@ -1,10 +1,18 @@
-use serenity::{async_trait, client::EventHandler};
+use serenity::{async_trait, client::{Context, EventHandler}, model::{channel::Message, prelude::Ready}};
 
 pub struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
 
-    
+    async fn ready(&self, _ctx: Context, ready: Ready) {
+        println!("Bot {} is ready", ready.user.name);
+    }
+
+    async fn message(&self, context: Context, message: Message) {
+        
+        
+
+    }
 
 }
