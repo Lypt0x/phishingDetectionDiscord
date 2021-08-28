39f92f21b49a1cfa74e59a2109d3f3ddc7b02695
Lypt0x <2xlyptox@gmail.com>
2021-08-28 13:35:20 +0200

    Start bot dynamically

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod bot;
+use std::borrow::Borrow;
+
 use bot::bot::*;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> bot::error::DynError<()> {
     
-    let bot = Bot::new("").await;
+    let mut bot = Bot::new(
+        std::env::args().nth(1).expect("No token has been defined").borrow()
+    ).await?;
 
+    match bot.start().await {
+        Ok(_) => println!("Bot stopped successfully"),
+        Err(why) => println!("Bot has been stopped with an error: {}", why)
+    }
+
+    Ok(())
 }
